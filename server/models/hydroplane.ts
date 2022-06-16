6f9b50a2410d3e638e89cabf0e9245dfcfb37b2c
victor morais <victor.morais@afinz.com.br>
2022-06-16 01:51:42 -0300

    fixed ships orientation

--- server/models/hydroplane.ts
+++ server/models/hydroplane.ts
@@ -5,11 +5,10 @@ import { Ship } from "./ship";
 export class HydroPlane extends Ship {
     constructor(initialPos: Point) {
         super('hydroplane', initialPos, [
-            [new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied)],
-            [new Block(BlockState.occupied), new Block(BlockState.occupied, 'hydroplane', initialPos), new Block(BlockState.occupied), new Block(BlockState.occupied)],
-            [new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied, 'hydroplane', initialPos.sumX(1).sumY(1), initialPos.sumX(2)), new Block(BlockState.occupied)],
-            [new Block(BlockState.occupied), new Block(BlockState.occupied, 'hydroplane', undefined, initialPos.sumX(1).sumY(1)), new Block(BlockState.occupied), new Block(BlockState.occupied)],
-            [new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied)]
-        ], initialPos.subX(1).subY(1), initialPos.sumX(3).sumY(1));
+            [new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied)],
+            [new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied, 'hydroplane', initialPos.sumX(1).sumY(1), initialPos.sumX(2)), new Block(BlockState.occupied), new Block(BlockState.occupied)],
+            [new Block(BlockState.occupied), new Block(BlockState.occupied, 'hydroplane', initialPos), new Block(BlockState.occupied), new Block(BlockState.occupied, 'hydroplane', undefined, initialPos.sumX(1).sumY(1)), new Block(BlockState.occupied)],
+            [new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied), new Block(BlockState.occupied)]
+        ], initialPos.subX(2).subY(1), initialPos.sumX(1).sumY(3));
     }
 }
